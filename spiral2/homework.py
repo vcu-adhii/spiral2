963f45efff9a3521fc3f4b4583d626bb35724917
Ibadadhi <72210975+Ibadadhi@users.noreply.github.com>
2021-05-02 18:10:23 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,17 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
-    return return_value
+    x = n  # my variables
+    y = 2
+    z = n
+    prev_step = 0
+
+    while z < pow(size, 2) + n - 1:  # spiralizing any array
+        z += y
+        x += z
+        prev_step += 1
+        if prev_step == 4:
+            y += 2
+            prev_step = 0
+    return x
+
+
+print(spiralize(481, 13))  # test statement
